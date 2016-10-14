e43c65f0847e8b454981e3a28f6af47330e24e78
kazatca <kazatca@gmail.com>
2016-10-14 23:11:15 +1000

    mvp

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,71 @@
 |
 */
 
+use App\User;
+use App\Transfer;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+  $users = User::all();
+  $transfers = Transfer::all();
+
+  return view('transfers',[
+      'users' => $users,
+      'transfers' => $transfers
+    ]);
+});
+
+
+Route::get('/users', function(){
+
+  $users = User::all();
+  return view('users', [
+    'users' => $users
+  ]);
+});
+
+Route::post('/user', function(Request $request){
+
+  $validator = Validator::make($request->all(), [
+    'name' => 'required|max:255',
+    'balance' => 'numeric'
+  ]);
+  
+  if ($validator->fails()) {
+    return redirect('/users')
+      ->withInput()
+      ->withErrors($validator);
+  }
+
+  $user = new User;
+  $user->name = $request->name;
+  $user->balance = $request->balance;
+  $user->save();
+
+  return redirect('/users');
+});
+
+Route::post('/transfer', function(Request $request){
+  $validator = Validator::make($request->all(), [
+    'from' => 'required',
+    'to' => 'required',
+    'volume' => 'min:0'
+  ]);
+
+  if ($validator->fails()) {
+    return redirect('/')
+      ->withInput()
+      ->withErrors($validator);
+  }
+
+  $transfer = new Transfer;
+
+  $transfer->from = $request->from;
+  $transfer->to = $request->to;
+  $transfer->volume = $request->volume;
+  $transfer->save();
+
+  return redirect('/');
+
+
 });
